f8b03a5e9b1265d3c69dcc4c66a313d008dcaf4e
ahmed <ahmedafatah@outlook.com>
2019-10-21 16:00:52 +0000

    add code in files types: .xml,.gradle,.java,.kt,.properties remove code in files types: .xml,.gradle,.java,.kt,.properties

--- content.java
+++ content.java
@@ -139,6 +139,6 @@ public class Crggga {
         System.out.println("gfdqu");
         System.out.println("stnwh");
         System.out.println("puwre");
-        System.out.println("jmvxm");
+        System.out.println("dokys");
     }
 }
