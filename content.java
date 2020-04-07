5339ef12096b26295894ea4dfeb20d64ab8436c5
ahmed <ahmedafatah@outlook.com>
2020-04-07 14:57:43 +0000

    add code in files types: .java,.kt remove code in files types: .kt

--- content.java
+++ content.java
@@ -147,5 +147,6 @@ public class Crggga {
         System.out.println("evove");
         System.out.println("rbvim");
         System.out.println("wirid");
+        System.out.println("gcnip");
     }
 }
