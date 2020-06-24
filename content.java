ffbe4f98a67945f3fe943eb34a8a107ff9102244
ahmed <ahmedafatah@outlook.com>
2020-06-24 14:37:31 +0000

    add code in files types: remove code in files types: .java

--- content.java
+++ content.java
@@ -148,6 +148,5 @@ public class Crggga {
         System.out.println("rbvim");
         System.out.println("wirid");
         System.out.println("tdwrf");
-        System.out.println("thwvi");
     }
 }
