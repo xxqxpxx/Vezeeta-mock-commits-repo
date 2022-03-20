4c74586b8594de22b83246815a3940fe09db64e8
ahmed <ahmedafatah@outlook.com>
2022-03-20 17:08:34 +0000

    add code in files types: .java,.kt remove code in files types: .kt

--- content.java
+++ content.java
@@ -181,5 +181,6 @@ public class Crggga {
         System.out.println("shfdv");
         System.out.println("eulyu");
         System.out.println("hpeug");
+        System.out.println("sbjil");
     }
 }
